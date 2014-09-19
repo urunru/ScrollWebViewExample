341584691a11b3a894005241acb4b9ba488d3ade
MURAKAMI <murakami@TgMacPro02-no-MacBook-Pro.local>
2014-09-19 15:17:54 +0900

    Initial Commit

---
+++ ScrollViewExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollViewExample
+//
+//  Created by MURAKAMI on 2014/09/19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollViewExampleTests/ScrollViewExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrollViewExampleTests.m
+//  ScrollViewExampleTests
+//
+//  Created by MURAKAMI on 2014/09/19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrollViewExampleTests : XCTestCase
+
+@end
+
+@implementation ScrollViewExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
